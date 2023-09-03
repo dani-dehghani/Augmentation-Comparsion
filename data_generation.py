f8224ac0b5fcfe4c80a0e9dfeaad3e5de23c3dce
Danial Dehghani <dehghani.dani97@gmail.com>
2023-09-04 01:15:32 +0200

    debug

--- data_generation.py
+++ data_generation.py
@@ -8,7 +8,7 @@ import random
 dataset_list = ['agnews','subj','pc','yelp','cr','cardio','bbc','sst2','pubmed','trec']
 aug_list = ['aeda', 'backtranslation', 'charswap', 'checklist', 'clare', 'deletion','eda', 'embedding', 'wordnet']
 aug_percent = [10,20,50]
-
+example_list = [1,2]
 #%%
 def data_percentage(dataset):
     print(f'{dataset} dataset')
@@ -46,13 +46,14 @@ def process_large_csv(testset, max_rows=200):
         # Save the selected rows as a new CSV file
         random_sample.to_csv(output_csv_path, index=False)
 
+            
 
 #%%
 
-
-def augmented_percent(dataset, aug_method, sampling_percentage):
+def augmented_example(dataset, aug_method, sampling_percentage, example=4):
     data_path = f'data/original/{dataset}'
     original_df = pd.read_csv(os.path.join(data_path, f'train_{sampling_percentage}_percent.csv'))
+    #original_df = pd.read_csv(os.path.join(data_path, 'train.csv'))
 
     augmented_path = f'data/augmented/{dataset}/{aug_method}'  # Corrected variable name
     augmented_df = pd.read_csv(os.path.join(augmented_path, f'meth_{aug_method}_pctwts_0.1_example_4.csv'))
@@ -63,56 +64,28 @@ def augmented_percent(dataset, aug_method, sampling_percentage):
     # Extract the indices of common rows and the next 4 rows
     indices_to_extract = []
     for index in common_rows.index:
-        if index + 4 < len(augmented_df):
-            indices_to_extract.extend(range(index, index + 5))
+        if index + example < len(augmented_df):
+            indices_to_extract.extend(range(index, index + example + 1))
 
     # Remove duplicates and create a new DataFrame
     indices_to_extract = list(set(indices_to_extract))
     df = augmented_df.iloc[indices_to_extract]
 
     # Define a file path for the output CSV file
-    output_csv_path = os.path.join(augmented_path, f'meth_{aug_method}_{sampling_percentage}_pctwts_0.1_example_4.csv')
+    output_csv_path = os.path.join(augmented_path, f'meth_{aug_method}_{sampling_percentage}_pctwts_0.1_example_{example}.csv')
+    #output_csv_path = os.path.join(augmented_path, f'meth_aeda_pctwts_0.1_example_{example}.csv')
+
     df.to_csv(output_csv_path, index=False)
 
-
-
 #%%
+
 
 if __name__ == '__main__':
     for name in dataset_list:
         for aug_method in aug_list:
             for percent in aug_percent:
-                augmented_percent(name,aug_method, percent)
-            
-
-#%%
-
-def augmented_example(dataset, aug_method, sampling_percentage, example):
-    data_path = f'data/original/{dataset}'
-    original_df = pd.read_csv(os.path.join(data_path, f'train_{sampling_percentage}_percent.csv'))
-
-    augmented_path = f'data/augmented/{dataset}/{aug_method}'  # Corrected variable name
-    augmented_df = pd.read_csv(os.path.join(augmented_path, f'meth_{aug_method}_pctwts_0.1_example_4.csv'))
-
-    # Find rows in the augmented dataset that are also in the original dataset
-    common_rows = augmented_df[augmented_df.isin(original_df.to_dict(orient='list')).all(1)]
-
-    # Extract the indices of common rows and the next 4 rows
-    indices_to_extract = []
-    for index in common_rows.index:
-        if index + 4 < len(augmented_df):
-            indices_to_extract.extend(range(index, index + 5))
-
-    # Remove duplicates and create a new DataFrame
-    indices_to_extract = list(set(indices_to_extract))
-    df = augmented_df.iloc[indices_to_extract]
-
-    # Define a file path for the output CSV file
-    output_csv_path = os.path.join(augmented_path, f'meth_{aug_method}_{sampling_percentage}_pctwts_0.1_example_4.csv')
-    df.to_csv(output_csv_path, index=False)
-
-
-
+                for i in example_list:
+                    augmented_example(name,aug_method, percent, i)
 
 
 
